509b47d5c9ef9be51821262576a3c3e4a8a2f213
Igor <igoraraujobda@gmail.com>
2023-03-28 16:10:01 -0300

    requisitos 7 e 8

---
+++ desafio8.sql
@@ -0,0 +1,3 @@
+-- Faça uma consulta na tabela employees que retorne o nome completo da pessoa colaboradora (colunas first_name e last_name) com o nome full_name e também a localização completa (colunas city, state_province e address) com o nome location.
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, CONCAT(city, '-', state_province, ', ', address) AS location FROM employees;
